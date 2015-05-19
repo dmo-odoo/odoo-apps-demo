e6a57b45cf1ab8314ac3b8cb0f72d2bd607d1276
David Monjoie <dmo@odoo.com>
2015-05-19 13:51:02 +0200

    [ADD] demo_module_again

---
+++ demo_module_again/__openerp__.py
@@ -0,0 +1,26 @@
+{
+    'name': 'A Demo Module, again',
+    'version': '1.0',
+    'category': 'Extra Tools',
+    'summary': 'Second demo module for Odoo Apps presentation at the Odoo Experience Days',
+    'description': """
+Another awesome module for the Odoo Apps demo
+=============================================
+
+This module is also a demo module and still doesn't add any feature.
+""",
+    'author': 'David Monjoie, Christophe Simonis',
+    'website': 'https://www.odoo.com/',
+    'depends': [
+        'base',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
