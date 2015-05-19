d6ac1d57cd40215fcd0937c43a3c52b7d94b2a1f
David Monjoie <dmo@odoo.com>
2015-05-19 14:03:03 +0200

    [ADD] theme_demo

---
+++ theme_demo/__openerp__.py
@@ -0,0 +1,29 @@
+{
+    'name': 'Demo Theme',
+    'version': '1.0',
+    'category': 'Theme/Corporate',
+    'summary': 'Awesome demo theme for Odoo Apps presentation at the Odoo Experience Days',
+    'description': """
+Awesome demo theme for the Odoo Apps demo
+=========================================
+
+Despite being really awesome, this theme is only a demo module and doesn't actually include a theme.
+""",
+    'author': 'David Monjoie, Christophe Simonis',
+    'website': 'https://www.odoo.com/',
+    'depends': [
+        'base',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'price': 199,
+    'currency': 'EUR',
+    'live_test_url': 'https://theme-demo.odoo.com',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
